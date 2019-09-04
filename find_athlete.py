c46accace23bc0ae66ab2f12988bdfe4b79170c1
pterodoctyl <52787519+pterodoctyl@users.noreply.github.com>
2019-09-04 23:44:29 +0300

    Update find_athlete.py

--- find_athlete.py
+++ find_athlete.py
@@ -1,4 +1,3 @@
-import uuid
 import datetime
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
@@ -8,105 +7,105 @@ DB_PATH = "sqlite:///sochi_athletes.sqlite3"
 
 Base = declarative_base()
 
+class Athelete(Base):
+    """
+    Описывает структуру таблицы athelete для хранения данных атлетов
+    """
+    __tablename__ = "athelete"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    age = sa.Column(sa.Integer)
+    birthdate = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    height = sa.Column(sa.Float)
+    name = sa.Column(sa.Text)
+    weight = sa.Column(sa.Integer)
+    gold_medals = sa.Column(sa.Integer)
+    silver_medals = sa.Column(sa.Integer)
+    bronze_medals = sa.Column(sa.Integer)
+    total_medals = sa.Column(sa.Integer)
+    sport = sa.Column(sa.Text)
+    country = sa.Column(sa.Text)
+
 class User(Base):
     """
-    Описывает структуру таблицы user для хранения регистрационных данных пользователей
+    Описывает структуру таблицы user для хранения данных пользователей
     """
-    __tablename__ = 'user'
-    id = sa.Column(sa.String(36), primary_key=True)
+    __tablename__ = "user"
+
+    id = sa.Column(sa.Integer, primary_key=True)
     first_name = sa.Column(sa.Text)
     last_name = sa.Column(sa.Text)
     email = sa.Column(sa.Text)
-
-
-class LastSeenLog(Base):
-    """
-    Описывает структуру таблицу log для хранения времени последней активности пользователя
-    """
-    # задаем название таблицы
-    __tablename__ = 'log'
-
-    # идентификатор пользователя, первичный ключ
-    id = sa.Column(sa.String(36), primary_key=True)
-    # время последней активности пользователя
-    timestamp = sa.Column(sa.DATETIME)
+    gender = sa.Column(sa.Text)
+    birthdate = sa.Column(sa.Text)
+    height = sa.Column(sa.Float)
 
 def connect_db():
     """
-    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии 
+    Устанавливает соединение с базой данных, создает таблицы, если их еще нет и возвращает обьект сессии
     """
-    # создаем соединение к базе данных
     engine = sa.create_engine(DB_PATH)
-    # создаем описанные таблицы
     Base.metadata.create_all(engine)
-    # создаем фабрику сессию
     session = sessionmaker(engine)
-    # возвращаем сессию
     return session()
 
-def find(name, session):
+def find(user_id, session):
     """
-    Производит поиск пользователя в таблице user по заданному имени name
+    Производит поиск пользователя в таблице user по заданному user_id и 
+    выводит на экран имена двух атлетов: ближайшего по дате рождения к
+    данному пользователю и ближайшего по росту
     """
-    # находим все записи в таблице User, у которых поле User.first_name совпадает с параметром name
-    query = session.query(User).filter(User.first_name == name)
-    # подсчитываем количество таких записей в таблице с помощью метода .count()
-    users_cnt = query.count()
-    # составляем список идентификаторов всех найденных пользователей
-    user_ids = [user.id for user in query.all()]
-    # находим все записи в таблице LastSeenLog, у которых идентификатор совпадает с одним из найденных
-    last_seen_query = session.query(LastSeenLog).filter(LastSeenLog.id.in_(user_ids))
-    # строим словарь вида идентификатор_пользователя: время_его_последней_активности
-    log = {log.id: log.timestamp for log in last_seen_query.all()}
-    # возвращаем кортеж количество_найденных_пользователей, список_идентификаторов, словарь_времени_активности
-    return (users_cnt, user_ids, log)
+    # находим запись в таблице User, у которой поле User.id совпадает с параметром user_id
+    user_check = session.query(User).get(user_id)
+    if user_check is not None:
+    
+        user_bd_str = session.query(User).get(user_id).birthdate
+        user_bd = datetime.date.fromisoformat(user_bd_str)  # конвертируем дату рождения пользователя в date
+        user_height = float(session.query(User).get(user_id).height)   # float
 
-def update_timestamp(user_id, session):
-    """
-    Обновляет время последней активности пользователя с заданным идентификатором user_id
-    """
-    # находим запись в журнале о пользователе с идентификатором user_id
-    log_entry = session.query(LastSeenLog).filter(LastSeenLog.id == user_id).first()
-    # проверяем, есть ли уже в журнале запись о таком пользователе
-    if log_entry is None:
-        # если записи не оказалось в журнале, создаем новую
-        log_entry = LastSeenLog(id=user_id)
+        # строим два словаря с разностями роста и двты рождения
+        ht_diff = {}    # dict athlet_id: height_difference
+        bd_diff = {}    # dict athlet_id: birhdate_difference in timedelta
+            
+        for atlet in session.query(Athelete).all():
+            if atlet.height is None:    # в базе есть атлеты с пустым полем height
+                atlet.height = '0'
+            ht_diff[atlet.id] = abs(float(atlet.height) - user_height)
+            atlet_bd = datetime.date.fromisoformat(atlet.birthdate)
+            bd_diff[atlet.id] = abs(atlet_bd - user_bd)
+            
+        # находим минимальные значения в словарях
+        closest_height_id = min(ht_diff, key=ht_diff.get) # вариант min(ht_diff, key=lambda k: ht_diff[k])
+        closest_birthdate_id = min(bd_diff, key=bd_diff.get)
+            
+        print("Ближайший по росту к пользователю атлет - "
+            f"{session.query(Athelete).get(closest_height_id).name}"
+            ", его рост - "
+            f"{session.query(Athelete).get(closest_height_id).height}"
+            " метра. Рост пользователя - "
+            f"{session.query(User).get(user_id).height} метра.")
+            
+        print("Ближайший по дате рождения к пользователю атлет -",
+            f"{session.query(Athelete).get(closest_birthdate_id).name}"
+            ", дата рождения - "
+            f"{session.query(Athelete).get(closest_birthdate_id).birthdate}"
+            ". Дата рождения пользователя - "
+            f"{session.query(User).get(user_id).birthdate}.")
 
-    # обновляем время последней активности пользователя на текущее
-    log_entry.timestamp = datetime.datetime.now()
-    return log_entry
-
-def print_users_list(cnt, user_ids, last_seen_log):
-    """
-    Выводит на экран количество найденных пользователей, их идентификатор и время последней активности.
-    Если передан пустой список идентификаторов, выводит сообщение о том, что пользователей не найдено.
-    """
-    # проверяем на пустоту список идентификаторов
-    if user_ids:
-        # если список не пуст, распечатываем количество найденных пользователей
-        print("Найдено пользователей: ", cnt)
-        # легенду будущей таблицы
-        print("Идентификатор пользователя - дата его последней активности")
-        # проходимся по каждому идентификатору
-        for user_id in user_ids:
-            # получаем время последней активности из словаря last_seen_log
-            last_seen = last_seen_log[user_id]
-            # выводим на экран идентификатор — время_последней_активности
-            print("{} - {}".format(user_id, last_seen))
     else:
-        # если список оказался пустым, выводим сообщение об этом
-        print("Пользователей с таким именем нет.")
+        print("Пользователей с таким id нет.")
 
 def main():
     """
-    Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
+    Осуществляет взаимодействие с пользователем и обрабатывает пользовательский ввод
     """
     session = connect_db()
-    name = input("Введи имя пользователя для поиска: ")
-        # вызываем функцию поиска по имени
-    users_cnt, user_ids, log = find(name, session)
-        # вызываем функцию печати на экран результатов поиска
-    print_users_list(users_cnt, user_ids, log)
-
+    user_id = int(input("Введите id пользователя для поиска: "))
+    
+    # вызываем функцию поиска по id пользователя
+    find(user_id, session)
+    
+    
 if __name__ == "__main__":
     main()
